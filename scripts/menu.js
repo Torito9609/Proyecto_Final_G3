0d6c1ae10f8c72d75c5caff18c556d75541ab0db
Torito9609 <toroandres85@gmail.com>
2025-02-07 17:09:52 -0500

    se oculta el botón de inicio y registro cuando hay un usuario loggeado

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,25 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerMenu = document.querySelector(".hamburger-menu");
+  const navLinks = document.querySelector(".nav-links");
+  const registo_inicio = document.getElementById("btn-login");
+  loged_user = JSON.parse(localStorage.getItem("logged_user"));
 
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
-    const navLinks = document.querySelector('.nav-links');
+  if (loged_user != undefined) {
+    registo_inicio.classList.add("hide-button");
+  }
 
-    // Alternar el menú al hacer clic en el botón del menú hamburguesa
-    hamburgerMenu.addEventListener('click', (event) => {
-        event.stopPropagation(); // Prevenir que el clic en el menú  cierre el menú
-        navLinks.classList.toggle('active');
-    });
+  // Alternar el menú al hacer clic en el botón del menú hamburguesa
+  hamburgerMenu.addEventListener("click", (event) => {
+    event.stopPropagation(); // Prevenir que el clic en el menú  cierre el menú
+    navLinks.classList.toggle("active");
+  });
 
-    // Para cerrar el menu cuando se hace clic en cualquier parte d ela pantalla
-    document.addEventListener('click', () => {
-        navLinks.classList.remove('active');
-    });
+  // Para cerrar el menu cuando se hace clic en cualquier parte d ela pantalla
+  document.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
 
-    // Cerrar el menú si se hace clic en uno de los enlaces dentro del menú
-    navLinks.addEventListener('click', (event) => {
-        if (event.target.tagName === 'A') {
-            navLinks.classList.remove('active');
-        }
-    });
-
-    
+  // Cerrar el menú si se hace clic en uno de los enlaces dentro del menú
+  navLinks.addEventListener("click", (event) => {
+    if (event.target.tagName === "A") {
+      navLinks.classList.remove("active");
+    }
+  });
 });
