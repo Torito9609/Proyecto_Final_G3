660b3174437b5c8f9d07a34007fd278a7ae8f658
Ana <anaforero91@gmail.com>
2025-02-09 22:17:09 -0500

    Aparece nombre al iniciar sesión

--- scripts/menu.js
+++ scripts/menu.js
@@ -5,7 +5,30 @@ document.addEventListener("DOMContentLoaded", () => {
   loged_user = JSON.parse(localStorage.getItem("logged_user"));
 
   if (loged_user != undefined) {
-    registo_inicio.classList.add("hide-button");
+    //registo_inicio.classList.add("hide-button");
+   // const element = document.getElementById("btn-login");
+   // element.remove();
+
+    let containerLogIn = document.getElementById("login-section");
+    let userData = JSON.parse(localStorage.getItem("logged_user"));
+
+    const userNameContainer = document.createElement('div');
+    userNameContainer.classList.add("user-name-container");
+    const userIcon = document.createElement('i');
+    userIcon.classList.add("bx");
+    userIcon.classList.add("bxs-user");
+    const userName = document.createElement('p');
+    userName.textContent = userData.name;
+
+    userNameContainer.appendChild(userIcon);
+    userNameContainer.appendChild(userName);
+    containerLogIn.appendChild(userNameContainer);
+  }else {
+    let containerLogIn = document.getElementById("login-section");
+    containerLogIn.innerHTML = `          
+    <a href="/html/inciar_sesiom.html">
+     <button id="btn-login">Registrarse / Iniciar Sesión</button>
+    </a>`;
   }
 
   // Alternar el menú al hacer clic en el botón del menú hamburguesa
