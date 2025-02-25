c5073f27da4d2c4716e3ed38dec05e2871af22c7
Ana <anaforero91@gmail.com>
2025-02-24 23:58:11 -0500

    Carrusel conectado con productos

--- scripts/productos_json.js
+++ scripts/productos_json.js
@@ -1,4 +1,7 @@
 //CONEXION CON EL BACKEND - PRODUCTOS
+
+let allProducts = [];
+
 async function loadProducts() {
   try {
    // const response = await fetch("/productos_json.json");
@@ -14,6 +17,9 @@ async function loadProducts() {
     console.log("Datos recibidos:",  data);
     let refactoredProductos = refactorJsonFromBackEnd(await data);
     displayProducts(refactoredProductos);
+
+    allProducts = refactoredProductos;
+  
   } catch (error) {
     console.error("Error:", error);
   }
@@ -139,6 +145,40 @@ function showModal(
   // Mostrar el modal
   modal.style.display = "flex";
 }
+
+async function openSavedProductModal() {
+  const selectedProductId = localStorage.getItem("selectedProduct");
+
+  if (!selectedProductId) return; // Si no hay producto guardado, no hacemos nada
+
+  // Esperar a que los productos se hayan cargado
+  if (allProducts.length === 0) {
+    console.log("Esperando a que los productos se carguen...");
+    await new Promise((resolve) => setTimeout(resolve, 500)); // Espera medio segundo
+  }
+
+  // Buscar el producto
+  const producto = allProducts.find((p) => p.id.toString() === selectedProductId);
+  console.log("Producto encontrado:", producto);
+
+  if (producto) {
+    showModal(
+      producto.nombre,
+      producto.imagen,
+      producto.descripción,
+      producto.categoría,
+      producto.cantidad,
+      producto.precio,
+      producto.id
+    );
+
+    // Limpiar el localStorage después de abrir el modal
+    localStorage.removeItem("selectedProduct");
+  } else {
+    console.log("No se encontró el producto con ID:", selectedProductId);
+  }
+}
+
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || []; // Inicializamos carrito desde localStorage si existe, de lo contrario, es un array vacío
 let productoSeleccionado = null; // Para almacenar el producto seleccionado
@@ -284,6 +324,7 @@ notificationClose.addEventListener("click", () => {
   notification.style.display = "none";
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  loadProducts();
+document.addEventListener("DOMContentLoaded", async () => {
+  await loadProducts();
+  openSavedProductModal();
 });
